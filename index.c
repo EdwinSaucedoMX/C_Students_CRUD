4c7b98cf8aa50f1fb01b3ceccbf699001bfd6252
Edwin Saucedo <edwin-saucedo@hotmail.com>
2022-11-24 07:12:23 -0600

    Adding gitignore & makin bin

--- index.c
+++ index.c
@@ -15,7 +15,8 @@ int main(){
     student_t student;
     while(!feof(db)){
         fscanf(db, "%s%d%s%d%f\n", student.name, &student.key, student.bachelor, &student.year, &student.average);
-        fprintf(bin, "%s  %d  %s  %d  %.2f\n", student.name, student.key, student.bachelor, student.year, student.average);
+        //printf("%s\t\t\t%d\t\t\t%s\t\t\t%d\t\t\t%.2f\n", student.name, student.key, student.bachelor, student.year, student.average);
+        //fwrite(&student, sizeof(student_t), 1, bin);
     }
 
     fclose(bin);
