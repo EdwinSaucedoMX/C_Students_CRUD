04fc35a063d3c5aa4d5f8522e1d45701205740c8
Edwin Donaldo Saucedo Vazquez <edwin-saucedo@hotmail.com>
2022-12-05 16:39:29 -0600

    Fixing values and adding comments for better understanding

--- main.c
+++ main.c
@@ -19,8 +19,7 @@ void print(bool data);
 void add_student();
 void remove_student();
 void sort_list();
-void duplicate_db();
-bool is_on_data(char *name);
+void backup_data();
 
 int main(){
 
@@ -30,17 +29,19 @@ int main(){
 
 int option;
 char str_option[30];
+
+//Asigning 1 to enter menu
 option = 1;
 bool database = true;
 
 while(option != 7){
     printf("\n1 : Buscar\n2 : Agregar Alumno\n3 : Eliminar Alumno\n4 : Imprimir Lista de Alumnos\n5 : Imprimir Datos\n6 : Ordenar\n7 : Salir\n\n");
 
+    //Reading as String to avoid errors
     scanf("%s", str_option);
+
+    //Converting String to Int
     option = atoi(str_option);
-    if(option == 0){
-        option = str_option[0] == '0' ? option : -1;
-    }
 
     printf("\n");
 
@@ -113,7 +114,7 @@ while(option != 7){
     }
 }
 
-    //Limpiar Archivos
+    //Clear output
     int rm;
     rm = remove("data");
     rm = remove("out");
@@ -121,10 +122,16 @@ while(option != 7){
     return 0;
 }
 
+/*
+* * Search Function
+*/
+
 void search(bool *db, int option){
     char name[30];
     FILE *read = *db ? fopen("database", "rb") : fopen("data", "rb");
 
+
+    //Checking if file exists
     if(!read){
         printf("No se encontraron datos almacenados\n");
         return;
@@ -152,6 +159,8 @@ void search(bool *db, int option){
     scanf("%s", name);
     float number = atof(name);
 
+
+    //Printing string according to option
     switch(option){
         case 1:
             printf("\nEstudiantes llamados %s\n\n", name);
@@ -165,7 +174,7 @@ void search(bool *db, int option){
             break;
     }
     
-    
+    //Changing database to data once a search is made
     if(*db){
         *db = false;
     }
@@ -180,32 +189,35 @@ void search(bool *db, int option){
 
     while(!feof(read)){
         fread(&student, sizeof(student_t), 1, read);
+
+        //Reading break just in case
         fscanf(read, "\n");
 
+        //Checking if student is found and printing it on screen and on file
 
         switch(option){
-            case 1:
+            case 1: //Name
                 if(strcasecmp(student.name, name) == 0){
                     fwrite(&student, sizeof(student_t), 1, write);
                     printf("%-15s %-6d %-25s %-8d %.2f\n", student.name, student.key, student.bachelor, student.year, student.average);
                     found = true;
                 }
                 break;
-            case 2:
+            case 2: //Bachelor
                 if(strcasecmp(student.bachelor, name) == 0){
                     fwrite(&student, sizeof(student_t), 1, write);
                     printf("%-15s %-6d %-25s %-8d %.2f\n", student.name, student.key, student.bachelor, student.year, student.average);
                     found = true;
                 }
                 break;
-            case 3:
+            case 3: //Year
                 if(student.year == (int)number){
                     fwrite(&student, sizeof(student_t), 1, write);
                     printf("%-15s %-6d %-25s %-8d %.2f\n", student.name, student.key, student.bachelor, student.year, student.average);
                     found = true;
                 }
                 break;
-            case 4:
+            case 4: //Average
                 if(student.average >= number){
                     //fprintf(write, "%s  %d  %s  %d  %.2f\n", student.name, student.key, student.bachelor, student.year, student.average);
                     fwrite(&student, sizeof(student_t), 1, write);
@@ -218,6 +230,8 @@ void search(bool *db, int option){
         }
     }
 
+    //Checking if student wasn't found and printing it on screen and clearing database boolean
+
     if(!found){
         printf("No se encontraron resultados de su busqueda\n\nReiniciando Filtros...\n");
         *db = true;
@@ -226,6 +240,7 @@ void search(bool *db, int option){
     fclose(read);
     fclose(write);
 
+    //To backup data
     save_data(false);
 }
 
@@ -239,6 +254,8 @@ void save_data(bool db){
         
         fread(&student, sizeof(student_t), 1, read);
         fwrite(&student, sizeof(student_t), 1, write);
+        
+        //Reading break just in case
         fscanf(read, "\n");
 
     }
@@ -248,6 +265,8 @@ void save_data(bool db){
 }
 
 void print(bool data){
+
+    //Checking wich file to read
     FILE *fp = !data ? fopen("database", "rb") : fopen("out", "rb");
     
     if(!fp){
@@ -261,13 +280,19 @@ void print(bool data){
 
     while(!feof(fp)){
         fread(&student, sizeof(student_t), 1, fp);
+
+        //Reading break just in case
         fscanf(fp, "\n");
         printf("%-2d : %-15s %-6d %-25s %-8d %.2f\n", count++, student.name, student.key, student.bachelor, student.year, student.average);
     }
 }
 
+/*
+* * Function to add a student to the database
+*/
+
 void add_student(){
-    duplicate_db();
+    backup_data();
     FILE *out = fopen("out", "ab");
 
     student_t student;
@@ -295,6 +320,10 @@ void add_student(){
     printf("\nEstudiante agregado\n");
 }
 
+/*
+* * Remove student from database function
+*/
+
 void remove_student(){
     char name[30];
     printf("Ingrese el nombre de el/los estudiante/s a eliminar: ");
@@ -309,6 +338,8 @@ void remove_student(){
 
     while(!feof(db)){
         fread(&student, sizeof(student_t), 1, db);
+
+        //Reading break just in case
         fscanf(db, "\n");
 
         if(strcasecmp(student.name, name) != 0){
@@ -330,9 +361,13 @@ void remove_student(){
     }
 }
 
+/*
+* * Function for sorting the database
+*/
+
 void sort_list(){
 
-    duplicate_db();
+    backup_data();
 
     printf("¿Que tipo de ordenamiento desea realizar?\n1 : Nombre\n2 : Carrera\n3 : Matricula\n4 : Año\n5 : Promedio\n6 : Regresar\n\n");
     int option;
@@ -343,15 +378,22 @@ void sort_list(){
     }
 
     FILE *data = fopen("out", "rb+");
+
+    //Declaring variables to swap
     student_t student;
     student_t minor;
 
     fseek(data, 0, SEEK_END);
 
+    //Reading last position
     long end = ftell(data), pos = 0, curr;
+
+    //Rewinding file
     rewind(data);
     char type[30];
 
+
+    //Printing according to option
     switch(option){
         case 1:
             strcpy(type, "Nombre");
@@ -373,16 +415,33 @@ void sort_list(){
     }
 
     printf("Ordenando por %s", type);
+
+    //Loop wich will read all the file and swap the minor with the current position
     while(ftell(data) <= end - sizeof(student_t)){
+
+        //Reading student according to position
         fread(&minor,sizeof(student_t), 1, data);
+
+        //Reading current position
         pos = ftell(data);
+
+
+        //While current position is less than end of file
         while(ftell(data) < end){
             fread(&student, sizeof(student_t), 1, data);
             
             switch(option){
-                case 1: 
+
+                    //Overwriting minor and current according to positions
+
+                case 1: //Name
                     if(strcasecmp(minor.name, student.name) > 0){
+                    
+
+                    //Current position inside loop
                     curr = ftell(data) - sizeof(student_t);
+
+                    //Overwriting minor and current
                     fseek(data, pos - sizeof(student_t), SEEK_SET);
                     fwrite(&student, sizeof(student_t), 1, data);
                     
@@ -392,7 +451,7 @@ void sort_list(){
                     minor = student;
                     }
                     break;
-                case 2: 
+                case 2: //Bachelor
                     if(strcasecmp(minor.bachelor, student.bachelor) > 0){
                     curr = ftell(data) - sizeof(student_t);
                     fseek(data, pos - sizeof(student_t), SEEK_SET);
@@ -404,7 +463,7 @@ void sort_list(){
                     minor = student;
                     }
                     break;
-                case 3: 
+                case 3: //Key
                     if(minor.key > student.key){
                     curr = ftell(data) - sizeof(student_t);
                     fseek(data, pos - sizeof(student_t), SEEK_SET);
@@ -416,7 +475,7 @@ void sort_list(){
                     minor = student;
                     }
                     break;
-                case 4: 
+                case 4: //Year
                     if(minor.year > student.year){
                     curr = ftell(data) - sizeof(student_t);
                     fseek(data, pos - sizeof(student_t), SEEK_SET);
@@ -428,7 +487,7 @@ void sort_list(){
                     minor = student;
                     }
                     break;
-                case 5: 
+                case 5: //Average
                     if(minor.average > student.average){
                     curr = ftell(data) - sizeof(student_t);
                     fseek(data, pos - sizeof(student_t), SEEK_SET);
@@ -446,13 +505,15 @@ void sort_list(){
             
         }
 
+        //Rewinding file according to position saved before
         fseek(data, pos, SEEK_SET);
     }
+
     fclose(data);
 
     printf("\nLista ordenada\n");
 
-
+    //Message to save changes on database
     printf("Desea guardar en la base de datos? (s/n): ");
 
     char c;
@@ -461,8 +522,13 @@ void sort_list(){
 
     bool save = c == 's' ? true : false;
 
+    //Save changes on the file selected
     save_data(save);
 }
+
+/*
+* * Swap function to swap two students
+*/
 
 void swap(student_t *a, student_t *b){
     student_t c;
@@ -471,24 +537,11 @@ void swap(student_t *a, student_t *b){
     *b = c;
 }
 
-bool is_on_data(char *name){
-    FILE *fp = fopen("out", "rb");
-    student_t student;
+/*
+* * Function to backup data on files
+*/
 
-    while(!feof(fp)){
-        fread(&student, sizeof(student_t), 1, fp);
-        fscanf(fp, "\n");
-
-        if(strcasecmp(student.name, name) == 0){
-            fclose(fp);
-            return true;
-        }
-    }
-    fclose(fp);
-    return false;
-}
-
-void duplicate_db(){
+void backup_data(){
     FILE *read = fopen("database", "rb");
     FILE *write = fopen("out", "wb+");
 
@@ -497,7 +550,8 @@ void duplicate_db(){
     while(!feof(read)){
         fread(&student, sizeof(student_t), 1, read);
         fwrite(&student, sizeof(student_t), 1, write);
-
+        
+        //Reading break just in case
         fscanf(read, "\n");
     }
 
